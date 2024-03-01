0481c468dc49c212f60b8a01ed322b5d791aeced
MOHAMMED RAFEEQ <mohammedrafeeqshariff17@gmail.com>
2024-03-01 15:03:55 +0530

    done todo

--- kalvium-todo/vite.config.js
+++ kalvium-todo/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/assignment-react-to-do-list-boilerplate.git/",
+  base:"/assignment-react-to-do-list-boilerplate",
   plugins: [react()],
 })
 https://github.com/mohammedrafeeqshariff/assignment-react-to-do-list-boilerplate.git
